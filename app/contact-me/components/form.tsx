7d56f40c0068d1900be7869646ff20e3cbe5b921
Aygün Bayır <82970249+aygunbyr@users.noreply.github.com>
2023-06-12 22:49:19 +0300

    feat: contact me page and form

---
+++ app/contact-me/components/form.tsx
@@ -0,0 +1,142 @@
+'use client'
+import { useState } from 'react'
+
+const Form = () => {
+  const [isMessageSent, setMessageSent] = useState<boolean>(false)
+
+  const [nameField, setNameField] = useState<string>('')
+  const [companyField, setCompanyField] = useState<string>('')
+  const [emailField, setEmailField] = useState<string>('')
+  const [messageField, setMessageField] = useState<string>('')
+
+  const handleChangeName = (e: React.FormEvent<HTMLInputElement>) =>
+    setNameField(e.currentTarget.value)
+
+  const handleChangeCompany = (e: React.FormEvent<HTMLInputElement>) =>
+    setCompanyField(e.currentTarget.value)
+
+  const handleChangeEmail = (e: React.FormEvent<HTMLInputElement>) =>
+    setEmailField(e.currentTarget.value)
+
+  const handleChangeMessage = (e: React.FormEvent<HTMLTextAreaElement>) =>
+    setMessageField(e.currentTarget.value)
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const target = e.currentTarget
+
+    const name = target.elements.namedItem('name') as HTMLInputElement
+    const company = target.elements.namedItem('company') as HTMLInputElement
+    const email = target.elements.namedItem('email') as HTMLInputElement
+    const message = target.elements.namedItem('message') as HTMLTextAreaElement
+
+    const data = {
+      name: name.value,
+      company: company.value,
+      email: email.value,
+      message: message.value,
+    }
+
+    try {
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+
+      setMessageSent(true)
+      setNameField('')
+      setCompanyField('')
+      setEmailField('')
+      setMessageField('')
+    } catch (error: any) {
+      console.log(
+        `There was a problem with the fetch operation: ${error.message}`
+      )
+    }
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="bg-white p-10">
+        <div className="mb-4">
+          <label className="label-form" htmlFor="name">
+            Name
+          </label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            value={nameField}
+            onChange={handleChangeName}
+            className="input-form"
+            required
+            minLength={3}
+            maxLength={200}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="label-form" htmlFor="company">
+            Company
+          </label>
+          <input
+            id="company"
+            type="text"
+            name="company"
+            value={companyField}
+            onChange={handleChangeCompany}
+            className="input-form"
+            minLength={2}
+            maxLength={200}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="label-form" htmlFor="email">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={emailField}
+            onChange={handleChangeEmail}
+            className="input-form"
+            required
+            minLength={2}
+            maxLength={200}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="label-form" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            value={messageField}
+            onChange={handleChangeMessage}
+            className="input-form"
+            required
+            minLength={10}
+            maxLength={1000}
+          />
+        </div>
+        <button type="submit" className="form-button">
+          Send Message
+        </button>
+      </form>
+      {isMessageSent && <p>Message has been sent</p>}
+    </>
+  )
+}
+
+export default Form
---
+++ app/contact-me/page.tsx
@@ -0,0 +1,23 @@
+import { contactMeText } from '../data/data'
+import Form from './components/form'
+
+const ContactMePage = () => {
+  const { title, body } = contactMeText
+
+  return (
+    <div className="flex flex-col w-11/12 content-center m-auto mt-10">
+      <h2>CONTACT ME</h2>
+      <div className="flex flex-row w-9/12 content-center m-auto bg-grey-light p-20 gap-20">
+        <div className="flex flex-col w-2/4">
+          <h3 className="text-2xl mb-10">{title}</h3>
+          <p>{body}</p>
+        </div>
+        <div className="w-2/4">
+          <Form />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ContactMePage
